68a818eb8ba9dd0c44e9b4c10a4469e752700cc9
Emmanuel-Kokkinos <Emmanuel.Kokkinos@hotmail.com>
2021-09-21 19:09:35 -0400

    Updated path visuals, changed enemy prefab, added enemy speed.

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -5,20 +5,32 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float waitTime = 1f;
+    [SerializeField] [Range(0f, 5f)] float speed = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(PrintWaypointName());
+        StartCoroutine(FollowPath());
     }
 
-    IEnumerator PrintWaypointName()
+
+    //Moves the enemy along the path
+    IEnumerator FollowPath()
     {
         foreach(Waypoint waypoint in path)
         {
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(waitTime);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPosition);
+
+            while (travelPercent < 1)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
