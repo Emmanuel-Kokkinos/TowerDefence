0e1bfe5c4afe82c3f2cb242913259d7b7dcfe4dd
Emmanuel-Kokkinos <Emmanuel.Kokkinos@hotmail.com>
2021-09-28 14:06:06 -0400

    Added point/money total UI

--- Assets/Bank/Bank.cs
+++ Assets/Bank/Bank.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
     [SerializeField] int startingBalance = 150;
+    [SerializeField] TextMeshProUGUI displayBalance;
 
     [SerializeField] int currentBalance;
     public int CurrentBalance { get { return currentBalance; } }
@@ -13,22 +15,30 @@ public class Bank : MonoBehaviour
     void Awake()
     {
         currentBalance = startingBalance;
+        UpdateDisplay();
     }
 
     public void Deposit(int amount)
     {
         currentBalance += Mathf.Abs(amount);
+        UpdateDisplay();
     }
 
     public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        UpdateDisplay();
 
-        if(currentBalance < 0)
+        if (currentBalance < 0)
         {
             //Lose the game
             ReloadScene();
         }
+    }
+
+    void UpdateDisplay()
+    {
+        displayBalance.text = "Gold: " + currentBalance;
     }
 
     void ReloadScene()
